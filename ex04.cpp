0bbba6353c6546a97ebdb1ae3f74070753ec3e9e
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-08-24 16:21:33 -0300

    resolvi o quarto exercicio da lista

---
+++ ex04.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout << "Digite o primeiro numero: ";
+    cin >> n1;
+    cout << "Digite o segunda numero: ";
+    cin >> n2;
+    cout << "A soma dos dois numeros e: " << n1 + n2;
+    return 0;
+}
