e3097ae12aa0042e6bcfd6468d1463aacb1b6c7d
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-08-24 16:17:59 -0300

    resolvi os dois primeiros exercicios de cpp

---
+++ ex03.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nome;
+    cout << "Digite o seu nome: ";
+    cin >> nome;
+    cout << "\nOla, " << nome << "\nComo voce esta?";
+    return 0;
+}
