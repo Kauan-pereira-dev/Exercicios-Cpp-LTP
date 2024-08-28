647c48f531af1f6acecedd5d9a8a58efd4f05428
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-08-28 18:31:35 -0300

    criação e resolução do exercicio 10

---
+++ ex010.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double pe, pol, jar, mil;
+    std::cout << "Digite o valor em pes: \n";
+    std::cin >> pe;
+    pol = 1;
+    jar = pe/3;
+    mil = jar/1760;
+    std::cout << "O valor de: \n" << pe << "\nA) Polegadas: " << pe * 12 * pol << "\nB) Jardas: " << jar << "\nC) Milhas: " << mil;
+}
