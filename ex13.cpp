4d9e73012923d328df34f1ab9676618165269049
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-09-08 21:38:13 -0300

    build: ex13

--- ex13.cpp
+++ ex13.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
-}
+    int cod;
+    double salario, NSal;
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=PERCENTUAL DE AUMENTO-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    cout << "Digite o seu codigo:\n";
+    cin >> cod;
+    cout << "Digite o seu salario:\n";
+    cin >> salario;
+    if (cod == 1){
+        NSal = (salario*0.5)+salario;
+        cout << "O seu cargo e: ESCRITURARIO.\nO aumento salarial e de 50%.\n Seu novo salario sera: R$" << NSal; 
+    }
+    else if (cod == 2){
+        NSal = (salario*0.35)+salario;
+        cout << "O seu cargo e: SECRETARIO(A).\nO aumento salarial e de 35%.\n Seu novo salario sera: R$" << NSal; 
+    }
+    else if (cod == 3){
+        NSal = (salario*0.2)+salario;
+        cout << "O seu cargo e: CAIXA.\nO aumento salarial e de 20%.\n Seu novo salario sera: R$" << NSal; 
+    }
+    else if (cod == 4){
+        NSal = (salario*0.1)+salario;
+        cout << "O seu cargo e: GERENTE.\nO aumento salarial e de 10%.\n Seu novo salario sera: R$" << NSal; 
+    }
+    else if (cod == 5){
+        NSal = (salario);
+        cout << "O seu caro e: DIRETOR\nNao houve aumento no seu salario.";
+    }
+    else {
+        cout << "O codigo digitado e invalido, redigite";
+    }
+    }   
