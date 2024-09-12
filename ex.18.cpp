7efabf86c15cc9d5740166c3c7f3fc25c1fae4e4
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-09-11 21:33:04 -0300

    build: ex18

---
+++ ex.18.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cont = 0;
+    for (cont = 0;cont <= 10;cont++){
+        cout << "1 x " << cont << " = " << 1*cont << "\n";
+    }
+
+}
