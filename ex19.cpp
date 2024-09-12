522b0a74806e252af0a699cb9215457c3f15dda3
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-09-11 21:52:27 -0300

    build and fix: ex19 fix; ex20 build

--- ex19.cpp
+++ ex19.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n, m;
-    cout << "TABUADA" << endl;
-    cout << "Digite o numero: ";
-    cin >> n;
 
-    for (m = 1; m <= 10; m++){
-        cout <<  n << " x " << n << " = " << n*1 << endl;
+int main() {
+    int numero;
+    cout << "Digite um número: ";
+    cin >> numero;
+
+    cout << "Tabuada do " << numero << ":\n";
+    for (int i = 1; i <= 10; ++i) {
+        cout << numero << " * " << i << " = " << numero * i << endl;
     }
+
+    return 0;
 }
