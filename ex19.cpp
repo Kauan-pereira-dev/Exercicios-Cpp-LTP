e8b99d107a113c17ba4c9abf71ea45dddcf66847
Kauan Oliveira Pereira <kauan.pereira121@outlook.com>
2024-09-11 21:45:49 -0300

    build: ex19

---
+++ ex19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, m;
+    cout << "TABUADA" << endl;
+    cout << "Digite o numero: ";
+    cin >> n;
+
+    for (m = 1; m <= 10; m++){
+        cout <<  n << " x " << n << " = " << n*1 << endl;
+    }
+}
